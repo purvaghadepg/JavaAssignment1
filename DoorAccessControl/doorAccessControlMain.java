c7756e266379d5488b20449b464eb2f64a2c13cf
purvaghadepg <147142117+purvaghadepg@users.noreply.github.com>
2024-09-27 02:40:09 +0530

    Add files via upload

---
+++ DoorAccessControl/doorAccessControlMain.java
@@ -0,0 +1,13 @@
+
+package JavaAssignment46;
+
+public class doorAccessControlMain {
+
+	public static void main(String[] args) {
+		doorAccessControl entry=new doorAccessControl();
+		entry.SetDetails();
+		entry.CheckAccessContro();
+
+	}
+
+}
